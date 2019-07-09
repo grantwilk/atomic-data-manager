e77f9ed8bc0106561bb1dfb25ea41a9268f86c13
Grant Wilk <grant@remingtongraphics.net>
2019-07-09 13:21:39 -0500

    Added config.py to store preferences for global access

---
+++ config.py
@@ -0,0 +1,29 @@
+"""
+Copyright (C) 2019 Grant Wilk
+
+This file is part of Atomic Data Manager.
+
+Atomic Data Manager is free software: you can redistribute
+it and/or modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation, either version 3 of the
+License, or (at your option) any later version.
+
+Atomic Data Manager is distributed in the hope that it will
+be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with Atomic Data Manager.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+# Atomic Data Manager Global Preferences
+# Do NOT edit these values!
+# Edit them in Blender's preferences instead
+
+# UI Preferences
+enable_missing_file_warning = False
+enable_stats_for_nerds = False
+
+# Other Preferences
+ignore_fake_users = False
